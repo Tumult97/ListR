9f40dfc1ed5e22a2b20602d12f71cab328744d50
Tristan van der Merwe <tristanvdm87@gmail.com>
2022-09-08 17:28:01 +0200

    Merge

--- listr-svc/ListR/ListR.Common/Interfaces/Repositories/IShoppingListRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using ListR.DataLayer.EntityModels.Lists;
-
-namespace ListR.Common.Interfaces.Repositories
-{
-    public interface IShoppingListRepository
-    {
-        public Task CreateShopList(ShopList model);
-    }
-}
